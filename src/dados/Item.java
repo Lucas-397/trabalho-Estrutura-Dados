162fe1ff9dc94016cd254dd44b8abf756fce5ac0
Lucas Machado <124196816+Lucas-397@users.noreply.github.com>
2025-06-12 19:23:54 -0300

    feat/organize-class-Item

--- src/dados/Item.java
+++ src/dados/Item.java
@@ -1,19 +1,53 @@
 package dados;
 
 public class Item {
-	private int chave;
+	private String nomeProduto;
+	private int codigoBarras;
+	private float precoUnitario;
+	private int quantidadeEstoque;
+	private String categoria;
+	
+	public Item(String nomeProduto, int codigoBarras, float precoUnitario, int quantidadeEstoque, String categoria) {
+		this.nomeProduto = nomeProduto;
+		this.codigoBarras = codigoBarras;
+		this.precoUnitario = precoUnitario;
+		this.quantidadeEstoque = quantidadeEstoque;
+		this.categoria = categoria;
+	}
+
+	public int getCodigoBarras() {
+		return codigoBarras;
+	}
+	public float getPrecoUnitario() {
+		return precoUnitario;
+	}
+	public int getQuantidadeEstoque() {
+		return quantidadeEstoque;
+	}
+	public void setCodigoBarras(int codigoBarras) {
+		this.codigoBarras = codigoBarras;
+	}
+	public void setPrecoUnitario(float precoUnitario) {
+		this.precoUnitario = precoUnitario;
+	}
+	public void setQuantidadeEstoque(int quantidadeEstoque) {
+		this.quantidadeEstoque = quantidadeEstoque;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
 	// aqui podem ser declarados outros atributos conforme sua necessidade.
 
 	//Construtor de objetos da classe Item
-	public Item(int ch) {
-		this.chave = ch;
-	}
 	//Modifica o valor do atributo chave
-	public void setChave (int ch){
-		this.chave = ch;
+	public void setNomeProduto (String ch){
+		this.nomeProduto = ch;
 	}
 	//Faz a leitura do valor do atributo chave
-	public int getChave (){
-		return this.chave;
+	public String getNomeProduto (){
+		return this.nomeProduto;
 	}
 }
